9fee1d6bda81c7de5f66fd58bcb9e33df6ea1bef
XM2Reginleif <jannkazuza@gmail.com>
2025-05-05 15:50:30 -0500

    05 de Mayo de 2025 V5

--- Login/server/index.js
+++ Login/server/index.js
@@ -31,17 +31,12 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRouter);
 
 // Servir archivos estáticos del frontend Vue
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
-// Para manejar rutas del lado del cliente (Vue Router)
-app.get('*', (req, res, next) => {
-  const indexPath = path.join(__dirname, 'dist', 'index.html');
-  res.sendFile(indexPath, (err) => {
-    if (err) {
-      next(err); // <-- Para que el error llegue a errorHandler
-    }
-  });
-});
+// 👉 CUALQUIER OTRA RUTA MANDA index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
 
 // Manejador de errores personalizado
 app.use(errorHandler);
