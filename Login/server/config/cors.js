98cbfbb8da9510907b3365bdff1cdfedbc1975f8
XM2Reginleif <jannkazuza@gmail.com>
2025-05-07 17:00:03 -0500

    07 de Mayo de 2025 V7

--- Login/server/config/cors.js
+++ Login/server/config/cors.js
@@ -12,5 +12,18 @@ export const credentials = (req, res, next) => {
     next();
 };
 
+import {allowedOrigins} from "./allowed_origins.js";
+
+export const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, origin);
+        } else {
+            callback("Error de CORS origin: " + origin + " No autorizado!");
+        }
+    }, 
+    credentials: true,
+};
+
 
 
