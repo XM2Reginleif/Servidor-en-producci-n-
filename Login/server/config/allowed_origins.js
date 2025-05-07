31a5def142efa7844ec381a03c5b28aec71721e0
XM2Reginleif <jannkazuza@gmail.com>
2025-05-07 15:17:48 -0500

    07 de Mayo de 2025 V2

--- Login/server/config/allowed_origins.js
+++ Login/server/config/allowed_origins.js
@@ -3,6 +3,7 @@ export const allowedOrigins = [
     "http://127.0.0.1:5500",
     "http://localhost:5000",
     "http://localhost:8080",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://servidor-te-pensamiento-computacional.onrender.com"
 ];
 
--- Login/server/config/cors.js
+++ Login/server/config/cors.js
@@ -1,14 +1,15 @@
-import {allowedOrigins} from "./allowed_origins.js";
+// middlewares/credentials.js
+import { allowedOrigins } from "../config/allowed_origins.js";
 
-export const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, origin);
-        } else {
-            callback("Error de CORS origin: " + origin + " No autorizado!");
-        }
-    }, 
-    credentials: true,
+export const credentials = (req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+    next();
 };
 
 
