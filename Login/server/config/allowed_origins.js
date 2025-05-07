5c4bf1832e3db7dae7fff7aec6bfde618d4e6b20
XM2Reginleif <jannkazuza@gmail.com>
2025-05-07 16:54:18 -0500

    07 de Mayo de 2025 V6

--- Login/server/config/allowed_origins.js
+++ Login/server/config/allowed_origins.js
@@ -4,5 +4,6 @@ export const allowedOrigins = [
     "http://localhost:5000",
     "http://localhost:8080",
     "http://localhost:5173",
+    "https://servidor-te-pensamiento-computacional.onrender.com"
 ]; 
 
