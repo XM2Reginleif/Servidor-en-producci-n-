a1668f86cc5815f783844a8fa5f6f6b48a53a8cc
XM2Reginleif <jannkazuza@gmail.com>
2025-05-07 19:15:31 -0500

    07 de Mayo de 2025 V10

--- Login/server/config/allowed_origins.js
+++ Login/server/config/allowed_origins.js
@@ -1,10 +1,17 @@
 
 export const allowedOrigins = [
+
+    // Desarrollo
     "http://127.0.0.1:5500",
     "http://localhost:5000",
     "http://localhost:8080",
     "http://localhost:5173",
+
+
+
+    //Production
     "https://servidor-te-pensamiento-computacional.onrender.com",
+    "https://servidor-en-producci-n-ltpr-pnl8qz5sh-julian-arcilas-projects.vercel.app",
     "https://servidor-en-producci-n-ltpr.vercel.app"
 ]; 
 
